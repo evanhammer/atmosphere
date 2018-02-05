1aa20a437a30ba693d9524df12b9af94712f73d9
Evan Hammer <evan@evanhammer.com>
2018-02-05 08:30:11 -0500

    Move extensions to Dropbox

--- .vscode/extensions/WallabyJs.wallaby-vscode-1.0.75/projects/55250fe3d36c4de9/instrumented/giant/delivery/client/scenes/People/List.jsx
+++
@@ -1,5 +0,0 @@
-$_$wp(46);
-import React from 'react';
-import PropTypes from 'prop-types';
-import List from 'material-ui/List';
-$_$wpe(46);
--- .vscode/extensions/ms-python.python-0.9.1/out/client/linters/types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ILinterHelper = Symbol('ILinterHelper');
-//# sourceMappingURL=types.js.map
