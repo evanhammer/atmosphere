dbc1e80260e63601383ad19e576f55b870e2657a
Evan Hammer <evan@evanhammer.com>
2024-08-22 09:14:44 -0400

    Reorganize applications in setup file and make applications list current

--- setup.sh
+++ setup.sh
@@ -28,19 +28,20 @@ brew install gh
 # ack
 brew install ack
 
-# nvm, node, & yarn
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-nvm install --lts --default # nvm install --lts (for latest)
-
-# openssl
-brew install openssl
-
-# wget
-brew install wget
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # check and do update
 # softwareupdate --list
 # softwareupdate -ia
+
+# make sure the `~/Dropbox/preferences` folder is downloaded before setting up
+# the other applications.
+brew install --cask dropbox
+open ~/Applications/Dropbox.app
+
+brew install --cask iterm2 # link to preferences manually
+# brew install --cask dash # link to preferences manually
 
 
 ###############################################################################
@@ -60,6 +61,7 @@ ln -s .atmosphere/.psqlrc .psqlrc
 ln -s .atmosphere/.vimrc .vimrc
 ln -s .atmosphere/.zshrc .zshrc
 
+
 ###############################################################################
 # HARDWARE
 ###############################################################################
@@ -68,95 +70,13 @@ brew install --cask karabiner-elements # ms keyboard
 ln -s ~/.atmosphere/karabiner.json ~/.config/karabiner/karabiner.json
 brew install --cask bettertouchtool # license in email, preferences in Dropbox
 
+
 ###############################################################################
 # FONTS
 ###############################################################################
 brew tap homebrew/cask-fonts
 brew install --cask font-input
 # open figma and set up fonts to sync
-
-
-###############################################################################
-# APPLICATIONS
-###############################################################################
-
-# prefer MAS over Caskroom for auto update
-brew install mas
-# sign into mac app store manually
-mas install 937984704 # aphemtamine (pause sleeping)
-mas install 1437226581 # horo (timer)
-mas install 462058435 # microsoft excel
-mas install 462054704 # microsoft word
-mas install 803453959 # slack
-mas install 506189836 # harvest
-mas install 1492280469 # preview markdown
-mas install 1320450034 # DaftCloud for SoundCloud
-# mas install 409203825 # numbers
-# mas install 409183694 # keynote
-# mas install 682658836 # garageband
-
-# basic
-brew install --cask 1password # not mas because qr scanner doesnt work
-brew install --cask dropbox
-brew install --cask google-chrome
-brew install --cask alfred
-brew install --cask textmate
-brew install --cask macdown # markdown
-
-# daily
-brew install --cask superhuman
-brew install --cask asana
-brew install --cask zoomus
-brew install --cask google-drive
-brew install --cask airtable
-brew install --cask obsidian
-brew install --cask vimcal
-
-# work
-brew install --cask logitune
-brew install --cask postman # HTTP Requests for API's
-brew install --cask skyfonts
-
-# personal
-brew install --cask spotify
-brew install --cask satellite-eyes # desktop background map
-brew tap homebrew/cask-drivers && brew install sonos
-brew install --cask daisydisk
-
-# autoraise
-# brew tap dimentium/autoraise
-# brew install autoraise
-# ln -s ~/.atmosphere/autoraise.config ~/.config/AutoRaise/config
-# brew services start autoraise
-
-# unsure
-# brew install --cask twist
-# brew install --cask rescuetime
-
-# make sure the `~/Dropbox/preferences` folder is downloaded before setting up
-# the other applications.
-open ~/Applications/Dropbox.app
-
-# preferences saved
-brew install --cask iterm2
-# link to preferences manually
-# brew install --cask dash # text expander
-
-# EBOOKS
-brew install --cask calibre
-brew install --cask adobe-digital-editions
-rm -rf ~/Library/Preferences/calibre
-ln -s ~/Dropbox/preferences/calibre ~/Library/Preferences/calibre
-# point calibre's library to ~/Dropbox/books
-
-# To Uninstall Applications, check these folders in both `/` and `~`:
-# Applications
-# Library
-# Library/Application Support
-# Library/LaunchAgents
-# Library/Preferences
-# Library/PreferencePanes
-# Library/StartupItems
 
 
 ###############################################################################
@@ -184,12 +104,92 @@ defaults write com.apple.PowerChime ChimeOnAllHardware -bool false
 defaults write com.apple.PowerChime ChimeOnNoHardware -bool true
 open -g -a PowerChime
 
+
+###############################################################################
+# APPLICATIONS
+###############################################################################
+
+# basic
+brew install --cask 1password # not mas because qr scanner doesnt work
+brew install --cask google-chrome
+brew install --cask alfred
+brew install --cask macdown # markdown
+
+# prefer MAS over Caskroom for auto update
+brew install mas
+# sign into mac app store manually
+mas install 937984704 # aphemtamine (pause sleeping)
+mas install 1437226581 # horo (timer)
+mas install 462058435 # microsoft excel
+mas install 462054704 # microsoft word
+mas install 803453959 # slack
+mas install 506189836 # harvest
+mas install 1492280469 # preview markdown
+mas install 1320450034 # DaftCloud for SoundCloud
+
+# daily
+brew install --cask superhuman
+brew install --cask zoomus
+brew install --cask google-drive
+brew install --cask airtable
+brew install --cask obsidian
+brew install --cask vimcal
+brew install timeular
+
+# Chrome: "Install page as app" for...
+# Asana Business
+# Asana Personal
+# ChatGPT
+
+# work
+brew install --cask logitune
+brew install notion
+brew install --cask postman # HTTP Requests for API's
+brew install --cask skyfonts
+
+# personal
+brew install --cask spotify
+brew install --cask satellite-eyes # desktop background map
+brew tap homebrew/cask-drivers && brew install sonos
+brew install --cask daisydisk
+brew install discord
+
+# autoraise
+# brew tap dimentium/autoraise
+# brew install autoraise
+# ln -s ~/.atmosphere/autoraise.config ~/.config/AutoRaise/config
+# brew services start autoraise
+
+# EBOOKS
+brew install --cask calibre
+brew install --cask adobe-digital-editions
+rm -rf ~/Library/Preferences/calibre
+ln -s ~/Dropbox/preferences/calibre ~/Library/Preferences/calibre
+# point calibre's library to ~/Dropbox/books
+
+# To Uninstall Applications, check these folders in both `/` and `~`:
+# Applications
+# Library
+# Library/Application Support
+# Library/LaunchAgents
+# Library/Preferences
+# Library/PreferencePanes
+# Library/StartupItems
+
+
 ###############################################################################
 # CODING ENVIRONMENT
 ###############################################################################
 
-# oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# nvm, node, & yarn
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+nvm install --lts --default # nvm install --lts (for latest)
+
+# openssl
+brew install openssl
+
+# wget
+brew install wget
 
 # setup vscode
 brew cask install visual-studio-code
@@ -245,11 +245,12 @@ brew install git bash-completion
 ###############################################################################
 
 # heroku
-brew tap heroku/brew && brew install heroku
+# brew tap heroku/brew && brew install heroku
 # requires xcode command line tools
 
 # digital ocean
-brew install doctl
+# brew install doctl
+
 
 ###############################################################################
 # PYTHON
@@ -275,10 +276,12 @@ sudo pip install pep8-naming
 # docutils
 # sudo pip install sphinx
 
+
 ###############################################################################
 # RUBY
 ###############################################################################
 gem install rubocop
+
 
 ###############################################################################
 # JAVASCRIPT
@@ -298,6 +301,7 @@ npm install -g eslint-plugin-react
 # jsctags
 # npm install -g git+https://github.com/ramitos/jsctags.git
 
+
 ###############################################################################
 # HTML/CSS/LESS
 ###############################################################################
@@ -305,10 +309,12 @@ npm install -g eslint-plugin-react
 # less
 # npm install less -g
 
+
 ###############################################################################
 # FIREBASE
 ###############################################################################
 npm install -g firebase-tools
+
 
 ###############################################################################
 # POSTGRES
@@ -333,4 +339,4 @@ gem install lunchy
 # ADD HSTORE EXTENSION: psql -d template1 -c 'create extension hstore;'
 
 # client: postico
-brew cask install postico
+brew install postico
